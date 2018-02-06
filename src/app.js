484ad20aa59c2823397a86934ec1bc7890d467c5
Vlad Plotnikov <vladimir.plotnikov89@gmail.com>
2018-02-05 16:00:43 -0800

    added babel support

---
+++ src/app.js
@@ -0,0 +1,7 @@
+console.log('App.js is running')
+
+//JSX - JavaScript XML
+
+const template = <p>This is JSX from app.js</p>
+const appRoot = document.getElementById('app')
+ReactDOM.render(template, appRoot);
