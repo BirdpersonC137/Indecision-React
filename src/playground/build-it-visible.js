bd5c9de65203165193a978ee4bdfda20baa0102b
Vlad Plotnikov <vladimir.plotnikov89@gmail.com>
2018-02-06 12:34:57 -0800

    reverted to defaults

--- src/playground/build-it-visible.js
+++ src/playground/build-it-visible.js
@@ -3,18 +3,19 @@ const app = {
 }
 
 const toggleVisibility = () =>{
-    if(app.visible){
-        app.visible = false
-    }else{
-        app.visible = true
-    }
+    // if(app.visible){
+    //     app.visible = false
+    // }else{
+    //     app.visible = true
+    // }
+    app.visible = !app.visible
     renderApp()
 }
 const renderApp = () =>{
     const template = (
         <div>
             <h1>Visibility Toggle</h1>
-            <button onClick={toggleVisibility}>Show details</button>
+            <button onClick={toggleVisibility}>{app.visible? 'Hide details':'Show details'}</button>
             {app.visible? <p>These are the details</p> : null}
         </div>
     )
