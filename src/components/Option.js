a53dc226fd10e91a172ea24960d4b89064ffeb8f
Vlad Plotnikov <vladimir.plotnikov89@gmail.com>
2018-02-16 14:49:25 -0800

    scss

--- src/components/Option.js
+++ src/components/Option.js
@@ -8,7 +8,9 @@ const Option = (props)=>(
             // it would return an event and pass it up as an argument
             //to handleDeleteOption in <App/> component, so we use the arrow function
             // with an event. This allows us to pass in the argument
+            
             // optionText up, which filter then utilizes.
+                className="button button--link"
                 onClick={(e)=>{
                     props.handleDeleteOption(props.optionText)
                 }}
--- src/components/Options.js
+++ src/components/Options.js
@@ -3,7 +3,9 @@ import Option from './Option'
 const Options = (props) => (
         <div>
         {props.options.length === 0 && <p>Please add an option to get started!</p>}
-        <button onClick={props.handleDeleteOptions}>Remove all</button>
+        <button 
+            className="button button--link"
+            onClick={props.handleDeleteOptions}>Remove all</button>
             {props.options.map((option)=>{
                 return (
                      <Option 
