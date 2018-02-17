375594172bf9440885c14e9cb64d2cda8618bcb3
Vlad Plotnikov <vladimir.plotnikov89@gmail.com>
2018-02-16 16:42:21 -0800

    finished basic app

--- src/components/AddOption.js
+++ src/components/AddOption.js
@@ -17,9 +17,11 @@ export default class AddOption extends Component{
     render(){
         return(
             <div>
-                {this.state.error && <p>{this.state.error}</p>}
-                <form onSubmit={this.handleSubmit}>
-                    <input type="text" name="option"/>
+                {this.state.error && <p className="add-option-error">{this.state.error}</p>}
+                <form 
+                    className="add-option"
+                    onSubmit={this.handleSubmit}>
+                    <input className="add-option__input" type="text" name="option"/>
                     <button className="button">Add Option</button>
                 </form>
             </div>
--- src/components/Options.js
+++ src/components/Options.js
@@ -2,15 +2,20 @@ import React from 'react';
 import Option from './Option'
 const Options = (props) => (
         <div>
-        {props.options.length === 0 && <p>Please add an option to get started!</p>}
-        <button 
-            className="button button--link"
-            onClick={props.handleDeleteOptions}>Remove all</button>
-            {props.options.map((option)=>{
+        <div className="widget-header">
+            <h3 className="widget-header__title">Your Options</h3>
+            <button 
+                className="button button--link"
+                onClick={props.handleDeleteOptions}>Remove all
+            </button>
+        </div>
+            {props.options.length === 0 && <p className="widget__message">Please add an option to get started!</p>}
+            {props.options.map((option, index)=>{
                 return (
                      <Option 
                         key={option} 
-                        handleDeleteOption={props.handleDeleteOption} 
+                        handleDeleteOption={props.handleDeleteOption}
+                        count={index+1} 
                         optionText={option} 
                      />
                 )
